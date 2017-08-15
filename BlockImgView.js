ff70d3f1ec2c0a872f96ec38b8a02cbb81e7c14c
noryom <noryom@163.com>
2017-08-15 10:42:00 +0800

    improve for showing ----------------- 优化显示效率，减少图片运算

--- BlockImgView.js
+++ BlockImgView.js
@@ -13,19 +13,32 @@ function BlockImgView(){
     this.outPt = [new ParamPoint()];
     var imageView = new ImageView();
 
+    var oldImgSrc;
+    var imgChanged = false;
     this.doAction = function(){
         this.outPt[0].value = this.inPt[0].value;
+
+        // init image from input
+        if(this.inPt[0].value && this.inPt[0].value.src){
+            if(!oldImgSrc){
+                imgChanged = true;
+            }else{
+                if(oldImgSrc!=this.inPt[0].value.src.substring(0,100)){
+                    imgChanged = true;
+                }
+            }
+            if(imgChanged){
+                oldImgSrc = this.inPt[0].value.src.substring(0,100);
+                imageView.initImg(this.inPt[0].value,this.w*1.5,this.h*1.5);
+                imgChanged = false;
+            }
+        }else{
+            imageView.clearImg();
+        }
     };
 
     this.rend = function(){
         this.parentType.rend.call(this);
-
-        // init image from input
-        if(this.inPt[0].value){
-            imageView.initImg(this.inPt[0].value,this.w*1.5,this.h*1.5);
-        }else{
-            imageView.clearImg();
-        }
 
         // image view
         imageView.setPos(this.x+this.w/2-imageView.getW()/2,this.y+this.h/2-imageView.getH()/2);
